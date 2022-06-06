b4965ef61a0321932064ea52ef8acb19b1855900
lunnex <ilyakorotkih62@gmail.com>
2022-06-06 03:15:02 +0300

    Add zones view and controller, add map

---
+++ TransportCompany/Controllers/TariffZonesController.cs
@@ -0,0 +1,26 @@
+﻿using BLL;
+using DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace TransportCompany.Controllers
+{
+    public class TariffZonesController : Controller
+    {
+
+        private readonly TariffZoneDAL _tariffZoneDAL;
+        private readonly TariffZoneBLL _tariffZoneBL;
+        private readonly IConfiguration _configuration;
+
+        public TariffZonesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _tariffZoneDAL = new TariffZoneDAL(_configuration.GetConnectionString("Database"));
+            _tariffZoneBL = new TariffZoneBLL(_tariffZoneDAL);
+        }
+        public IActionResult Index()
+        {
+            return View(_tariffZoneBL.GetAll());
+        }
+    }
+}
